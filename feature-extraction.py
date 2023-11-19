9c95c5189cfb6e1bd5dd8d88e7bde2db3570504d
JM <jacobmontpetit@gmail.com>
2023-11-19 14:25:37 -0500

    FeatureX: re-org to input string cli arg

--- feature-extraction.py
+++ feature-extraction.py
@@ -31,15 +31,15 @@ def extractAndAdd(website):
     }
 
 
-def NotCommentOrEmpty(line):
+def notCommentOrEmpty(line):
     """
     helper function to sanitize lines
     returns bool if line is not comment or empty
     """
-    return site[0] != "#" and len(site) > 0 and site[0] != "\n"
+    return line[0] != "#" and len(line) > 0 and line[0] != "\n"
 
 
-def ExtractFeatures(websiteDict, fn):
+def extractFeatures(websiteDict, fn):
     """
     creates additional dictionary of {key + func name : func applied}
     this is essentially a list map function for dictionaries
@@ -52,18 +52,19 @@ def ExtractFeatures(websiteDict, fn):
     return extracted_features
 
 
-def apply(datalist, part, fn):
-    return {"{}_{}".format(part, fn): [fn(d[part]) for d in datalist]}
-
-
-def getPickle(pickle_file):
-    features = None
-    if os.path.isfile(pickle_file):
-        with open(pickle_file, "rb") as pickled_obj:
-            features = pickle.load(pickled_obj).to_dict()
-    else:
-        features = {}
-    return features
+def extractDatFromFile(file):
+    """
+    reads in file removes line ending and seperates the Url into fields
+    w/extractAndAdd
+    returns list of 'extractAndAdd dictionaries'
+    """
+    cleanizedData = []
+    with open(file, "r") as fin:
+        for site in fin.readlines():
+            if notCommentOrEmpty(site):
+                site = site.replace("\n", "")
+                cleanizedData.append(extractAndAdd(site))
+    return cleanizedData
 
 
 """
@@ -80,47 +81,65 @@ features = {
 }
 
 
-parser = argparse.ArgumentParser(
-    prog="Feature Extraction",
-    description="extracts features from urls and output pandas dataframe",
-)
-
-parser.add_argument(
-    "DataDirectory",
-    type=pathlib.Path,
-)
-parser.add_argument("-i", type=pathlib.Path, help="pickle file imported")
-parser.add_argument("-o", type=pathlib.Path, help="pickle file output")
-parser.add_argument("-c", type=int, help="class label to apply on pickle file")
-
-if __name__ == "__main__":
-    args = parser.parse_args(os.sys.argv[1:])
-
-    cleanedRawData = []
-    for datafile in os.listdir(args.DataDirectory):
-        print("********** {} ***********".format(datafile))
-        with open("{}/{}".format(args.DataDirectory, datafile), "r") as fin:
-            for site in fin.readlines():
-                if NotCommentOrEmpty(site):
-                    site = site.replace("\n", "")
-                    cleanedRawData.append(extractAndAdd(site))
-
-    extractedFeatures = {}
-    if args.i:
-        extractedFeatures = getPickle(args.i)
-
-    for k in features:
-        for fn in features[k]:
+def applyMeasurements(featureMap, cleanedRawData, extractedFeatures):
+    for k in featureMap:
+        for fn in featureMap[k]:
             extractedFeatures["{}_{}".format(k, fn.__name__)] = [
                 fn(x[k]) for x in cleanedRawData
             ]
-    # add the class label
-    extractedFeatures["classification"] = [args.c] * len(
-        list(extractedFeatures.values())[0]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Feature Extraction",
+        description="extracts features from urls and output pandas dataframe",
     )
+
+    parser.add_argument(
+        "data",
+        type=str,
+    )
+    parser.add_argument(
+        "-i", "--input", type=pathlib.Path, help="pickle file imported", default=None
+    )
+    parser.add_argument(
+        "-o", "--output", type=pathlib.Path, default=None, help="pickle file output"
+    )
+    parser.add_argument("-c", type=int, help="class label to apply on pickle file")
+    parser.add_argument("-d", action="store_true", help="Directory input flag")
+    parser.add_argument("-f", action="store_true", help="File input flag")
+
+    args = parser.parse_args(os.sys.argv[1:])
+
+    cleanedRawData = []
+    if args.d:
+        for datafile in os.listdir(args.data):
+            print("********** {} ***********".format(datafile))
+            dataFileStr = "{}/{}".format(args.DataDirectory, datafile)
+            cleanedRawData += extractDatFromFile(dataFileStr)
+    elif args.f:
+        # just open file and extract to pickle
+        cleanedRawData += extractDatFromFile(args.data)
+    else:
+        cleanedRawData += [extractAndAdd(args.data)]
+
+    extractedFeatures = {}
+    if args.input:
+        extractedFeatures = pandas.read_pickle(args.input).to_dict()
+
+    applyMeasurements(features, cleanedRawData, extractedFeatures)
+
+    # add label for training classification
+    if args.c:
+        # add the class label
+        extractedFeatures["classification"] = [args.c] * len(
+            list(extractedFeatures.values())[0]
+        )
 
     # put into a pandas dataframe
 
-    if args.o:
-        with open(args.o, "wb") as dataOut:
+    if args.output:
+        with open(args.output, "wb") as dataOut:
             pickle.dump(pandas.DataFrame(extractedFeatures), dataOut)
+    else:
+        print(extractedFeatures)
