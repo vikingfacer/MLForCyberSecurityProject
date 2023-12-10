8392c592a6f44b04b6912676d77a4ec43a990101
JM <jacobmontpetit@gmail.com>
2023-12-09 19:57:02 -0500

    plot features: update to histogram all features

--- plot_features.py
+++ plot_features.py
@@ -1,37 +1,53 @@
 #!/usr/bin/env python3
 
+import argparse
 import matplotlib.pyplot as plot
-import pickle
+import pandas
 import os
 
+
+def makeDataSet(benign, target):
+    # benign class is a giant set
+    # there for only a portion equal to the size of target is selected
+    benign_sample = benign.sample(n=int(target.size / len(target.columns)))
+
+    # combine the two sets
+    model_data = pandas.concat([target, benign_sample])
+
+    # Step to move classification to Y from X
+    Y = model_data.classification
+    X = model_data.drop("classification", axis=1)
+
+    return X, Y
+
+
 if __name__ == "__main__":
-    with open(os.sys.argv[1], "rb") as fin:
-        data = pickle.load(fin)
-    features = {
-        "website_len": [],
-        "subdomain_len": [],
-        "domain_len": [],
-        "tld_len": [],
-        "website_entropy": [],
-        "subdomain_entropy": [],
-        "domain_entropy": [],
-        "tld_entropy": [],
-        "website_metric_entropy": [],
-        "subdomain_metric_entropy": [],
-        "domain_metric_entropy": [],
-        "tld_metric_entropy": [],
-        "website_CCR": [],
-        "subdomain_CCR": [],
-        "domain_CCR": [],
-        "tld_CCR": [],
-    }
+    parser = argparse.ArgumentParser(
+        prog="Plot Feature",
+        description="Plot the dataset features",
+    )
+    parser.add_argument("target", help="Pickled Pandas Dataframe of target")
+    parser.add_argument("benign", help="Pickled Pandas Dataframe of benign")
+    parser.add_argument("-o", help="Output directory for figures", default=None)
 
-    for v in data.values():
-        for k in features.keys():
-            features[k].append(v[k])
+    args = parser.parse_args(os.sys.argv[1:])
+    with open(args.target, "rb") as fin:
+        ads = pandas.read_pickle(fin)
 
-    for k, v in features.items():
-        print("plotting {}".format(k))
-        plot.scatter(range(len(v)), v)
-        plot.savefig("{}.png".format(k))
+    with open(args.benign, "rb") as fin:
+        ben = pandas.read_pickle(fin)
+
+    # redefine ben
+    ben = ben.sample(n=int(ads.size / len(ads.columns)))
+
+    features = list(ads.columns)
+
+    for feat in features:
+        pandas.DataFrame({"ads": ads[feat], "benign": ben[feat]}).plot.hist(
+            alpha=0.5, title=feat
+        )
+        if args.o:
+            plot.savefig("{}/{}.png".format(args.o, feat))
+        else:
+            plot.savefig("{}.png".format(feat))
         plot.clf()
